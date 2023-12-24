e30b75aeb0d01bc39f59d74fbf1cb1244bed74da
mehul1409 <mehulbansalswm1234@gmail.com>
2023-12-24 12:59:11 +0530

    add team member section

--- src/Components/Team/Team.js
+++ src/Components/Team/Team.js
@@ -1,12 +1,111 @@
-import React from 'react'
-import './Team.css'
+import React from "react";
+import "./Team.css";
+import { FaGithub } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
 
 const Team = () => {
-  return (
-    <div>
-      <div>TEAM PAGE</div>
-    </div>
-  )
-}
+  const TeamMember = [
+    {
+      img: "/images/mehul.jpg",
+      name: "Mehul Bansal",
+      role: "Frontend Developer",
+      social: [
+        "https://github.com/mehul1409",
+        "https://www.instagram.com/mehulbansal1409",
+        "https://www.linkedin.com/in/mehul-bansal-27173b254",
+      ],
+    },
+    {
+      img: "/images/mehul.jpg",
+      name: "Mehul Bansal",
+      role: "Frontend Developer",
+      social: [
+        "https://github.com/mehul1409",
+        "https://www.instagram.com/mehulbansal1409",
+        "https://www.linkedin.com/in/mehul-bansal-27173b254",
+      ],
+    },
+    {
+      img: "/images/mehul.jpg",
+      name: "Mehul Bansal",
+      role: "Frontend Developer",
+      social: [
+        "https://github.com/mehul1409",
+        "https://www.instagram.com/mehulbansal1409",
+        "https://www.linkedin.com/in/mehul-bansal-27173b254",
+      ],
+    },
+    {
+      img: "/images/mehul.jpg",
+      name: "Mehul Bansal",
+      role: "Frontend Developer",
+      social: [
+        "https://github.com/mehul1409",
+        "https://www.instagram.com/mehulbansal1409",
+        "https://www.linkedin.com/in/mehul-bansal-27173b254",
+      ],
+    },
+    {
+      img: "/images/mehul.jpg",
+      name: "Mehul Bansal",
+      role: "Frontend Developer",
+      social: [
+        "https://github.com/mehul1409",
+        "https://www.instagram.com/mehulbansal1409",
+        "https://www.linkedin.com/in/mehul-bansal-27173b254",
+      ],
+    },
+    {
+      img: "/images/mehul.jpg",
+      name: "Mehul Bansal",
+      role: "Frontend Developer",
+      social: [
+        "https://github.com/mehul1409",
+        "https://www.instagram.com/mehulbansal1409",
+        "https://www.linkedin.com/in/mehul-bansal-27173b254",
+      ],
+    },
+  ];
 
-export default Team
+  return (
+    <>
+    <div className="TeammemberHeading">TEAM MEMBERS</div>
+      <div className="container">
+        {TeamMember.map((member, index) => (
+          <div className="card" key={index}>
+            <div className="content">
+              <div className="imgbox">
+                <img src={member.img}></img>
+              </div>
+              <div className="contentbox">
+                <h3>{member.name}</h3>
+                <br />
+                <p>{member.role}</p>
+              </div>
+            </div>
+            <ul className="social">
+              <li>
+                <a href={member.social[0]}>
+                  <FaGithub />
+                </a>
+              </li>
+              <li>
+                <a href={member.social[1]}>
+                  <FaInstagram />
+                </a>
+              </li>
+              <li>
+                <a href={member.social[2]}>
+                  <FaLinkedin />
+                </a>
+              </li>
+            </ul>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default Team;
